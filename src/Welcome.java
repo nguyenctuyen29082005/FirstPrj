a3301ea10b8b90d349aa5a6302ee306c7eb73563
nguyenctuyen29082005 <nguyenctuyen29082005@gmail.com>
2024-08-13 20:34:41 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Cong Tuyen!");
+        System.out.println("Welcome to the DHKTPM19A");
+
+    }
+}
